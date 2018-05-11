03c5847f2219446f7f29db87d42ed52e10d8a3f3
sander <sanderdalm@hotmail.com>
2018-05-11 18:23:55 +0200

    Virtual batch norm + momentum implemented.

--- es_learner.py
+++ es_learner.py
@@ -1,12 +1,14 @@
 import numpy as np
 from optimizers import AdamOptimizer
-from virtual_batch_norm import VirtualBatchNorm
+
+
 class ESLearner:
 
     def __init__(self,
                  input_dims,
                  output_dims,
-                 hidden_size,
+                 hidden_size1,
+                 hidden_size2,
                  std,
                  output_lower_bound,
                  output_upper_bound,
@@ -14,28 +16,25 @@ class ESLearner:
                  alpha,
                  pop,
                  env,
-                 discrete,
-                 use_VBN=True):
+                 discrete):
 
         self.input_dims = input_dims
         self.output_dims = output_dims
         self.output_lower_bound = output_lower_bound
-        self.upper_lower_bound = output_upper_bound
+        self.output_upper_bound = output_upper_bound
         self.sigma = sigma
         self.alpha = alpha
         self.pop = pop
         self.env = env
         self.discrete = discrete
         self.params =   {
-                        'w1': np.random.normal(0, std, [input_dims, hidden_size]),
-                        'gamma1': np.random.normal(1, std, [hidden_size, 1]),
-                        'b1': np.zeros([hidden_size, 1]),
+                        'w1': np.random.normal(0, std, [input_dims, hidden_size1]),
+                        'b1': np.zeros([hidden_size1, 1]),
 
-                        'w2': np.random.normal(0, std, [hidden_size, hidden_size]),
-                        'gamma2': np.random.normal(1, std, [hidden_size, 1]),
-                        'b2': np.zeros([hidden_size, 1]),
+                        'w2': np.random.normal(0, std, [hidden_size1, hidden_size2]),
+                        'b2': np.zeros([hidden_size2, 1]),
 
-                        'w3': np.random.normal(0, std, [hidden_size, output_dims]),
+                        'w3': np.random.normal(0, std, [hidden_size2, output_dims]),
                         'b3': np.zeros([output_dims, 1])
                         }
 
@@ -43,15 +42,10 @@ class ESLearner:
         for key in self.params.keys():
            self.optimizers[key] = AdamOptimizer(self.params[key])
 
-        self.use_VBN = use_VBN
-        if self.use_VBN:
-            self.VBN = VirtualBatchNorm()
 
-
-    def load_params(self, params, VBN=None):
+    def load_params(self, params):
         self.params = params
-        if VBN:
-            self.VBN = VBN
+
 
     def generate_noise(self, x):
         noise = np.random.normal(0, self.sigma, [x.shape[0], x.shape[1]])
@@ -59,11 +53,10 @@ class ESLearner:
 
 
     def activation(self, x):
-        return np.tanh(x)
-        #return np.maximum(0, x)
+        return np.maximum(0, x)
 
 
-    def model_no_VBN(self, x, params):
+    def model(self, x, params):
 
         z1 = np.matmul(params['w1'].T, x) + params['b1']
         a1 = self.activation(z1)
@@ -71,30 +64,11 @@ class ESLearner:
         z2 = np.matmul(params['w2'].T, a1) + params['b2']
         a2 = self.activation(z2)
 
-        if self.discrete:
-            return np.argmax(self.activation(np.matmul(params['w3'].T, a2) + params['b3'])), z1, z2
-        else:
-            return np.clip(np.matmul(params['w3'].T, a2) + params['b3'], self.output_lower_bound,
-                           self.upper_lower_bound), z1, z2
-
-
-    def model_VBN(self, x, params):
-
-        z1 = np.matmul(params['w1'].T, x)
-        z1 = self.VBN.normalize_activations(z1, 'z1')
-        z1 = self.VBN.denormalize_activations(z1, params['gamma1'], params['b1'])
-        a1 = self.activation(z1)
-
-
-        z2 = np.matmul(params['w2'].T, a1)
-        z2 = self.VBN.normalize_activations(z2, 'z2')
-        z2 = self.VBN.denormalize_activations(z2, params['gamma2'], params['b2'])
-        a2 = self.activation(z2)
 
         if self.discrete:
             return np.argmax(self.activation(np.matmul(params['w3'].T, a2) + params['b3']))
         else:
-            return np.clip(np.matmul(params['w3'].T, a2) + params['b3'], self.output_lower_bound, self.upper_lower_bound)
+            return np.clip(np.matmul(params['w3'].T, a2) + params['b3'], self.output_lower_bound, self.output_upper_bound)
 
 
     def run_episode(self, params):
@@ -105,50 +79,13 @@ class ESLearner:
         x = observation.reshape([self.input_dims, 1])
 
         while not done:
-            if self.use_VBN:
-                action = self.model_VBN(x, params)
-            else:
-                action, _, _ = self.model_no_VBN(x, params)
+
+            action = self.model(x, params)
             observation, reward, done, info = self.env.step(action)
             x = observation.reshape([self.input_dims, 1])
             episode_reward += reward
 
         return episode_reward
-
-
-    def collect_batch_norm_statistics(self):
-
-        x_list = []
-        z1_list = []
-        z2_list = []
-
-        print('Collecting virtual batchnorm statistics.')
-
-        for i in range(self.pop):
-            done = False
-            observation = self.env.reset()
-            x = observation.reshape([self.input_dims, 1])
-            x_list.append(x)
-
-            while not done:
-                action, z1, z2 = self.model_no_VBN(x, self.params)
-                observation, reward, done, info = self.env.step(action)
-                z1_list.append(z1)
-                z2_list.append(z2)
-                x = observation.reshape([self.input_dims, 1])
-                x_list.append(x)
-
-        z1 = np.array(z1_list)
-        z2 = np.array(z2_list)
-
-        z1_mean = np.mean(z1, axis=0)
-        z2_mean = np.mean(z2, axis=0)
-
-        z1_std = np.std(z1, axis=0)
-        z2_std = np.std(z2, axis=0)
-
-        self.VBN.update_stats(z1_mean, z1_std, 'z1')
-        self.VBN.update_stats(z2_mean, z2_std, 'z2')
 
 
     def compute_ranks(self, x):
--- train.py
+++ train.py
@@ -1,19 +1,23 @@
 import pickle
 import json
+from time import sleep
 
 import numpy as np
+import roboschool
 import gym
 import matplotlib.pyplot as plt
 
 from es_learner import ESLearner
 
 
-config = json.load(open('config/humanoid.json', 'rb'))
+config = json.load(open('config/humanoid.json'))
 env = gym.make(config['env_name'])
+
 
 learner = ESLearner(input_dims=config['input_size'],
                     output_dims=config['output_size'],
-                    hidden_size=config['hidden_units'],
+                    hidden_size1=config['hidden_units_1'],
+                    hidden_size2=config['hidden_units_2'],
                     std=config['initialization_std'],
                     output_lower_bound=config['lower_bound'],
                     output_upper_bound=config['upper_bound'],
@@ -21,14 +25,11 @@ learner = ESLearner(input_dims=config['input_size'],
                     alpha=config['alpha'],
                     pop=config['N'],
                     env=env,
-                    discrete=config['discrete'],
-                    use_VBN=config['VBN']
+                    discrete=config['discrete']
                     )
 
-#learner.collect_batch_norm_statistics()
-
-params = pickle.load(open('params/Humanoid-v1_5700', 'rb'))
-learner.load_params(params)
+#params = pickle.load(open('params/Humanoid', 'rb'))
+#learner.load_params(params)
 
 reward_list = []
 num_gens = config['num_generations']
@@ -38,29 +39,23 @@ for gen in range(num_gens):
     reward_list.append(rewards)
     print('Mean reward after {} generations: {}'.format(gen, np.mean(rewards)))
     print('Mean W1: {}'.format(np.mean(np.abs(params['w1']))))
-    if gen % 100 == 0 or gen == num_gens-1:
-        #pickle.dump((params, learner.VBN), open('params/{}_{}'.format(config['env_name'], gen), 'wb'))
+    if gen % 1000 == 0 or gen == num_gens-1:
         pickle.dump(params, open('params/{}_{}'.format(config['env_name'], gen), 'wb'))
-
-learner.optimizers['w1'].rms
 
 plt.plot(reward_list)
 plt.show()
-#plt.savefig('lr.01.png')
-#plt.savefig('lr.001.png')
+# plt.savefig('cheetah.png')
 
-
-for i in range(15):
+for i in range(1):
     episode_reward = 0
     done = False
     observation = env.reset()
     x = observation.reshape([config['input_size'], 1])
     while not done:
-        env.render()
-        #action = learner.model_VBN(x, learner.params)
-        action, _, _ = learner.model_no_VBN(x, learner.params)
+        #env.render()
+        action = learner.model(x, learner.params)
         observation, reward, done, info = env.step(action)
         x = observation.reshape([config['input_size'], 1])
         episode_reward += reward
     print(episode_reward)
-env.close()
+print('Mean W1: {}'.format(np.mean(np.abs(action))))
